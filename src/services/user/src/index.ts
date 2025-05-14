92752fa2d1fff6b8075f3496ffc363eeefe98609
mdarkanurl <mdarkanurl@gmail.com>
2025-05-13 20:32:47 -0800

    auth service basic boilerplate

---
+++ src/services/user/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+config();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middlewares
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+    const origin = req.headers.origin || '';
+
+    if(allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        next();
+    } else {
+        res.status(403).json({ message: 'Forbidden' });
+    }
+});
+
+// Router
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`);
+});
