c6d343c48c86d3a2229e19fceb9a744d377e6e0f
mdarkanurl <mdarkanurl@gmail.com>
2025-05-13 21:20:09 -0800

    user schema define

--- src/services/user/src/schemas.ts
+++ src/services/user/src/schemas.ts
@@ -1,1 +1,11 @@
 import { z } from "zod";
+
+export const userCreateSchema = z.object({
+    authUserId: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    address: z.string().optional(),
+    phone: z.string().optional()
+});
+
+export const userUpdateSchema = userCreateSchema.omit({ authUserId: true }).partial();
