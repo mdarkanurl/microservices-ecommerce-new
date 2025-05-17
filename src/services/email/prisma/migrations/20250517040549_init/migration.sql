63a45102f587e2cc4957c4688f101c5fedaa6736
mdarkanurl <mdarkanurl@gmail.com>
2025-05-16 20:07:30 -0800

    Database setup done for Email service

---
+++ src/services/email/prisma/migrations/20250517040549_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Email" (
+    "id" TEXT NOT NULL,
+    "recipient" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "source" TEXT NOT NULL,
+    "sendAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Email_pkey" PRIMARY KEY ("id")
+);
