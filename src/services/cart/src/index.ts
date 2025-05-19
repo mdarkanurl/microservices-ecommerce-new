1b11a027391e1eff2e7b261f981f88b658daab29
mdarkanurl <mdarkanurl@gmail.com>
2025-05-18 18:42:15 -0800

    Boilerplate done for cart service

---
+++ src/services/cart/src/index.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { config } from "dotenv";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+
+config();
+
+const app = express();
+
+// security middleware
+app.use(helmet());
+
+// Rate limiting middleware
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+    handler: (_req, res) => {
+        res
+            .status(429)
+            .json({ message: 'To many requests, please try again later' });
+    }
+});
+
+app.use('/api', limiter);
+
+// request logger
+app.use(morgan('dev'));
+app.use(express.json());
+
+// health check
+app.get('/api/health', (_req: Request, res: Response) => {
+    res.json({ message: 'API Gateway is running' });
+});
+
+app.use((err: ErrorRequestHandler, _req: Request, res: any, _next: NextFunction) => {
+    console.log(err);
+    return res.status(500).json({ message: 'Internal Server Error' });
+});
+
+const PORT = process.env.PORT || 4006;
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`);
+});
